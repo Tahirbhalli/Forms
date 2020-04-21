ba9fd0c9b751cfc7256c9d03bab58d45cc2ef907
SpaYco <zizou.aziz101@gmail.com>
2020-04-21 14:47:09 +0100

    added #show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
 class UsersController < ApplicationController
 
+    def index
+      @list = User.all
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+      @user = User.find(params[:id])
+      user.username = :username
+      user.password = :password
+      user.email = :email
+        
+        if @user.save
+          redirect_to new_user_path
+        else
+          render :edit
+        end
+    end
+
     def new
         @user = User.new
     end
