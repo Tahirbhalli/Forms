55037454232ba6925d1a1dd5a996bdecefe357e2
SpaYco <zizou.aziz101@gmail.com>
2020-04-21 10:47:25 +0100

    added a for with the form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
 
     def new
+        @user = User.new
     end
     
     def create
-        @user = User.new(user_params)
+        @user = User.new(@User)
         
         if @user.save
           redirect_to new_user_path
@@ -12,4 +13,11 @@ class UsersController < ApplicationController
           render :new
         end
     end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
