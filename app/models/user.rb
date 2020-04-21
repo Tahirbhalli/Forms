d21e20a4ba9d7ff8b76a35d95b6b255bcc90b3d7
tahirbhalli <tahirbhalli16@gmail.com>
2020-04-21 14:19:33 +0500

    validations problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: { case_sensitive: true }, length: {maximum: 20}
-    validates :email, presence: true, uniqueness: { case_sensitive: true }, length: {maximum: 20}, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :email, presence: true, uniqueness: { case_sensitive: true }, length: {maximum: 50}, confirmation: true
     validates :password, presence: true, length: {minimum: 10,maximum: 50}
     
     
