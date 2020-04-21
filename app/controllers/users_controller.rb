7543c79cf93a9f37c672b7b5726d3ff8f14e2b3c
tahirbhalli <tahirbhalli16@gmail.com>
2020-04-21 17:58:40 +0500

    formfor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
     end
     
     def create
-        @user = User.new(@User)
+      @user = User.new(user_params)
         
         if @user.save
           redirect_to new_user_path
