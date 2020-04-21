164b2a4a4c5418b1695b353467c4020ff339fb70
tahirbhalli <tahirbhalli16@gmail.com>
2020-04-21 19:54:28 +0500

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,4 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: { case_sensitive: true }, length: {maximum: 50}, confirmation: true
     validates :password, presence: true, length: {minimum: 10,maximum: 50}
     
-    
-    
-    
-    
 end
