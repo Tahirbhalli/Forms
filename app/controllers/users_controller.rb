d5d261cc26b54c213d0aafb7ada949f21978512f
SpaYco <zizou.aziz101@gmail.com>
2020-04-22 08:41:20 +0100

    fixed rubocop issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,41 @@
 class UsersController < ApplicationController
+  def index
+    @list = User.all
+  end
 
-    def index
-      @list = User.all
+  def show; end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
     end
+  end
 
-    def show
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
+  end
 
-    def edit
-      @user = User.find(params[:id])
-    end
+  private
 
-    def update
-      @user = User.find(params[:id])
-      
-        
-        if @user.update(user_params)
-           redirect_to user_path
-        else
-          render :edit
-        end
-    end
-
-    def new
-        @user = User.new
-    end
-    
-    def create
-      @user = User.new(user_params)
-        
-        if @user.save
-          redirect_to new_user_path
-        else
-          render :new
-        end
-    end
-
-    private
-
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
-
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
